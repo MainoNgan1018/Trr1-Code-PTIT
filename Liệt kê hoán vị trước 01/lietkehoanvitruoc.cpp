324522a49de1dfc149133d3d8cd18a3b960e96fa
Hoang._.Ngan <dangthihoangngan108@gmail.com>
2026-07-08 14:35:57 +0700

    Add files via upload

---
+++ Liệt kê hoán vị trước 01/lietkehoanvitruoc.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin>>n;
+    vector<int> s(n);
+    for(int i=0;i<n;i++){
+        cin>>s[i];
+    }
+    int h=s.size();
+    int i= h-2;
+    while(i>=0 && s[i]<=s[i+1]) i--;
+    if(i<0) cout<<"0\n";
+    else{
+        int j=h-1;
+        while(j>i && s[j]>=s[i]) j--;
+        swap(s[i],s[j]);
+        reverse(s.begin()+i+1,s.end());
+        for(auto x:s){
+            cout<<x<<" ";
+        }
+    }
+} 
